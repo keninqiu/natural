521dad2f38a7c54a4d42f42b13b8694e73882cf3
Kening Qiu <keningqiu@Kenings-MacBook-Pro.local>
2016-12-31 12:51:03 -0500

    fix

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=airi.clticau8zr5w.us-west-2.rds.amazonaws.com;dbname=surroundnatural',
-    'username' => 'surround',
-    'password' => 'fwedew*&4^%',
-    'charset' => 'utf8',
-];
