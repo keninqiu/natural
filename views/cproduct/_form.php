e88d3d47498a950f61fa287a687b27d230a74da9
Kening Qiu <keningqiu@Kenings-MacBook-Pro.local>
2016-12-31 13:17:11 -0500

    fix

--- views/cproduct/_form.php
+++ views/cproduct/_form.php
@@ -2,7 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use yii\helpers\ArrayHelper;
+use app\models\Catalog;
+use app\models\Product;
 /* @var $this yii\web\View */
 /* @var $model app\models\CatalogProduct */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,9 +14,15 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'catalog_id')->textInput() ?>
+	<?= $form->field($model, 'catalog_id')
+     ->dropDownList(
+            ArrayHelper::map(Catalog::find()->asArray()->all(), 'id', 'name_zh')
+            ) ?>
 
-    <?= $form->field($model, 'product_id')->textInput() ?>
+	<?= $form->field($model, 'product_id')
+     ->dropDownList(
+            ArrayHelper::map(Product::find()->asArray()->all(), 'id', 'name_zh')
+            ) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
